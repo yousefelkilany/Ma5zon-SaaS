06d7345feb492f68495df94f6e5048786abeb516
Youssef Elkilany <youssof.kilaney@gmail.com>
2026-06-08 12:49:27 +0300

    feat(router): add tanstack router config

---
+++ src/router/index.ts
@@ -0,0 +1,58 @@
+import { createRouter, createRoute } from '@tanstack/react-router'
+import { MainWindowContent } from '@/components/layout/MainWindowContent'
+import { EntityWorkspace } from '@/components/entity'
+import { DashboardContent, NewTabContent } from '@/components/tabs'
+
+const rootRoute = createRoute({
+  path: '/',
+  component: () => null,
+})
+
+const entityRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/entity/:entityType',
+  component: EntityWorkspace,
+})
+
+const dashboardRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/dashboard',
+  component: DashboardContent,
+})
+
+const newTabRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/new-tab',
+  component: NewTabContent,
+})
+
+const salesInvoiceRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/sales-invoice',
+  component: NewTabContent,
+})
+
+const purchaseInvoiceRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/purchase-invoice',
+  component: NewTabContent,
+})
+
+const catchAllRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '*',
+  component: DashboardContent,
+})
+
+export const routeTree = rootRoute.addChildren([
+  dashboardRoute,
+  newTabRoute,
+  salesInvoiceRoute,
+  purchaseInvoiceRoute,
+  entityRoute,
+  catchAllRoute,
+])
+
+export const router = createRouter({
+  routeTree,
+})
