28ef76e87ee2f6adf300e8bd379bc5d8576f0492
Youssef Elkilany <youssof.kilaney@gmail.com>
2026-06-08 19:18:52 +0300

    fix: import App.css in main.tsx to restore theme CSS

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { RouterProvider } from '@tanstack/react-router'
 import { router } from '@/router'
+import './App.css'
 import './i18n'
 import { queryClient } from './lib/query-client'
 
